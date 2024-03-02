f46f324a749d1d76342f4eec9cc8ad3034f7806f
Quincy Bowers <quincy@users.noreply.github.com>
2024-03-02 06:43:02 -0700

    Bootstrap project

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+  kotlin("jvm") version "1.9.22"
+  id("idea")
+  id("io.ktor.plugin") version "2.3.8"
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+  id("io.gitlab.arturbosch.detekt").version("1.23.3")
+  id("com.diffplug.spotless") version "6.25.0"
+}
+
+group = "com.quakbo"
+
+version = "0.0.1"
+
+application {
+  mainClass.set("com.quakbo.ApplicationKt")
+
+  val isDevelopment: Boolean = project.ext.has("development")
+  applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories { mavenCentral() }
+
+dependencies {
+  implementation("io.ktor:ktor-server-content-negotiation-jvm")
+  implementation("io.ktor:ktor-server-core-jvm")
+  implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+  implementation("io.ktor:ktor-server-call-logging-jvm")
+  implementation("io.ktor:ktor-server-call-id-jvm")
+  implementation("io.ktor:ktor-server-cors-jvm")
+  implementation("io.ktor:ktor-server-compression-jvm")
+  implementation("io.ktor:ktor-server-host-common-jvm")
+  implementation("io.ktor:ktor-server-status-pages-jvm")
+  implementation("io.ktor:ktor-server-resources")
+  implementation("io.ktor:ktor-server-auto-head-response-jvm")
+  implementation("io.ktor:ktor-server-sessions-jvm")
+  implementation("io.ktor:ktor-server-auth-jvm")
+  implementation("io.ktor:ktor-server-auth-jwt-jvm")
+  implementation("io.ktor:ktor-server-netty-jvm")
+  implementation("io.ktor:ktor-server-webjars-jvm")
+  implementation("org.webjars:jquery:3.7.1")
+
+  implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+  // test
+  testImplementation("io.ktor:ktor-server-tests-jvm")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
+
+idea { module { isDownloadSources = true } }
+
+spotless {
+  // optional: limit format enforcement to just the files changed by this feature branch
+  ratchetFrom("origin/main")
+
+  kotlin { ktfmt() }
+
+  format("misc") {
+    // define the files to apply `misc` to
+    target("*.gradle", ".gitattributes", ".gitignore")
+
+    // define the steps to apply to those files
+    trimTrailingWhitespace()
+    indentWithSpaces(2)
+    endWithNewline()
+  }
+}
---
+++ src/main/kotlin/com/quakbo/plugins/HTTP.kt
@@ -0,0 +1,30 @@
+package com.quakbo.plugins
+
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.compression.Compression
+import io.ktor.server.plugins.compression.deflate
+import io.ktor.server.plugins.compression.gzip
+import io.ktor.server.plugins.compression.minimumSize
+import io.ktor.server.plugins.cors.routing.CORS
+
+@Suppress("MagicNumber")
+fun Application.configureHTTP() {
+  install(CORS) {
+    allowMethod(HttpMethod.Options)
+    allowMethod(HttpMethod.Put)
+    allowMethod(HttpMethod.Delete)
+    allowMethod(HttpMethod.Patch)
+    allowHeader(HttpHeaders.Authorization)
+    allowHost("0.0.0.0")
+  }
+  install(Compression) {
+    gzip { priority = 1.0 }
+    deflate {
+      priority = 10.0
+      minimumSize(1024) // condition
+    }
+  }
+}
